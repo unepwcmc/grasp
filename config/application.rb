a2735c33f8c79ba61bf1f8bac05f732bc4e23651
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-06-29 20:06:50 +0100

    configure browserify in application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module Grasp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.browserify_rails.paths << /vendor\/assets\/javascripts/
+    config.browserify_rails.commandline_options = %q{-t coffee-reactify --extension=".cjsx"}
+    config.browserify_rails.source_map_environments << "development"
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
