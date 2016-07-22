381c1ec1ce9b9150b693e7be418f14328592ecdd
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-07-22 11:13:42 +0200

    add user belongs to report

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,7 +6,13 @@
 #  data       :json
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_reports_on_user_id  (user_id)
 #
 
 class Report < ActiveRecord::Base
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719162209) do
+ActiveRecord::Schema.define(version: 20160722073421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20160719162209) do
     t.json     "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "reports", ["user_id"], name: "index_reports_on_user_id", using: :btree
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
