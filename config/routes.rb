6ad5a717ba762c37f1d8918a6051751b9091d66d
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-08-03 16:54:16 +0100

    Added buttons for better navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :agencies
   devise_for :users, controllers: { registrations: "registrations" }
-  root 'home#index'
+  root 'reports#index'
 
   resources :reports
 
