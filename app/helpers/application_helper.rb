fb005b43cb72a79c9936bbeceebe839e21da3f53
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-08-25 13:28:56 +0100

    Added javascript notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
       concat(path ? link_to(text, path) : text)
     end
   end
+
+  def alert_key_to_fa_icon key
+    case key
+    when "success" then "check"
+    when "error" then "times-circle"
+    else
+      "info-circle"
+    end
+  end
 end
