d5960574e66f31b83ef07775d9bad0607b34a672
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-09-02 16:41:51 +0100

    Fixed the search parameters listing page

--- lib/modules/params_utils.rb
+++ lib/modules/params_utils.rb
@@ -5,7 +5,7 @@ module ParamsUtils
 
   def self.strip_ordering_params(params)
     params = strip_rails_defaults(params)
-    params.except(:sort, :dir)
+    params.except(:sort, :dir, :page)
   end
 
   def self.strip_empty(params)
