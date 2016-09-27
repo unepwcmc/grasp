3a8706528938eb5ca195ab561ffeabded3bc9494
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-09-27 17:26:54 +0100

    add first gorilla tooltips

--- lib/modules/questionnaire.rb
+++ lib/modules/questionnaire.rb
@@ -17,4 +17,12 @@ module Questionnaire
   def self.page name
     ERB.new(File.read(QUESTIONNAIRE_ROOT.join("pages", "#{name}.json.erb"))).result(binding)
   end
+
+  def self.image name
+    ActionController::Base.helpers.asset_path(name)
+  end
+
+  def self.tooltip name
+    ERB.new(File.read(QUESTIONNAIRE_ROOT.join("tooltips", "#{name}.html.erb"))).result(binding).gsub('"', '\"')
+  end
 end
